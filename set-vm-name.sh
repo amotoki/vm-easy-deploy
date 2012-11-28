b147b1b1257509636986edc521d113c186797032
Akihiro MOTOKI <motoki@da.jp.nec.com>
2012-11-28 20:42:36 +0900

    Make the LVM handling logic more flexible
    The previous logic is specific to Ubuntu. This commit enables to
    detect a LVM partition and a logical volume for the root partition.
    By this commit CentOS image can be handled now.
    
    Limitations:
    - The logic cannot handle the image which has multiple Linux LVM partition
      It uses a partition found first.
    - A name of Logical volume for the root partition must contain 'root'.

--- set-vm-name.sh
+++ set-vm-name.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
 NBD_DEV=/dev/nbd0
-NBD_PART=${NBD_DEV}p5
-ROOT_LV=root
 MOUNT_PATH=/mnt
 
 if [ -z "$2" ]; then
@@ -31,9 +29,10 @@ if ! echo $IMAGE | grep -E '^/'; then
 fi
 /usr/bin/qemu-nbd -c $NBD_DEV $IMAGE
 
-if fdisk -l $NBD_DEV | grep $NBD_PART | grep LVM; then
+if fdisk -l $NBD_DEV | grep 'Linux LVM'; then
   # LVM
   TYPE=lvm
+  NBD_PART=`fdisk -l $NBD_DEV | grep 'Linux LVM' | head -1 | awk '{print $1;}'`
   # refresh Volume Information
   #/sbin/vgscan
   /sbin/pvscan
@@ -43,19 +42,34 @@ if fdisk -l $NBD_DEV | grep $NBD_PART | grep LVM; then
     exit 10
   fi
   /sbin/vgchange -ay $VG
+  ROOT_LV=`ls -1 /dev/$VG | grep root`
   mount /dev/$VG/$ROOT_LV $MOUNT_PATH
 else
   # non-LVM
   TYPE=normal
+  # It is true for Ubuntu 12.04 but need to check other distros.
+  NBD_PART=${NBD_DEV}p5
   mount $NBD_PART $MOUNT_PATH
 fi
 
 #-------------------
 # Modify VM Image
 #-------------------
-OLD_HOSTNAME=`cat $MOUNT_PATH/etc/hostname`
-sed -i -e "s|$OLD_HOSTNAME|$NAME|g" $MOUNT_PATH/etc/hostname
-sed -i -e "s|$OLD_HOSTNAME|$NAME|g" $MOUNT_PATH/etc/hosts
+if [ -f $MOUNT_PATH/etc/debian_version ]; then
+  HOSTNAME_FILE=$MOUNT_PATH/etc/hostname
+  if [ -f $HOSTNAME_FILE ]; then
+    OLD_HOSTNAME=`cat $HOSTNAME_FILE`
+    sed -i -e "s|$OLD_HOSTNAME|$NAME|g" $HOSTNAME_FILE
+    sed -i -e "s|$OLD_HOSTNAME|$NAME|g" $MOUNT_PATH/etc/hosts
+  fi
+elif [ -f $MOUNT_PATH/etc/redhat-release ]; then
+  HOSTNAME_FILE=$MOUNT_PATH/etc/sysconfig/network
+  if [ -f $HOSTNAME_FILE ]; then
+    OLD_HOSTNAME=`grep HOSTNAME $HOSTNAME_FILE | cut -d = -f 2`
+    sed -i -e "s|$OLD_HOSTNAME|$NAME|g" $HOSTNAME_FILE
+    sed -i -e "s|$OLD_HOSTNAME|$NAME|g" $MOUNT_PATH/etc/hosts
+  fi
+fi
 
 #-------------------
 # Finishing stage
