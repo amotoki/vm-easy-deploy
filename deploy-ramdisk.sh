de665fba765a34f85c9742ad541c203bf3f2816f
Akihiro Motoki <amotoki@gmail.com>
2015-03-11 17:11:54 +0900

    Add alias scripts

---
+++ deploy-ramdisk.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <host>"
+    exit 1
+fi
+HOST=$1
+
+IMAGE=ubuntu1404.img
+
+./easy_deploy.py --use-ramdisk --no-hostname -c 4 -m 8 -i pub $HOST $IMAGE
