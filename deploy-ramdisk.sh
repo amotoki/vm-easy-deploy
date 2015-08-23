3600c6b3d086761bbf1c754c17ec25547172ce02
Akihiro Motoki <amotoki@gmail.com>
2015-08-23 17:29:24 +0900

    deploy.sh: work with abspath

--- deploy-ramdisk.sh
+++ deploy-ramdisk.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TOP_DIR=$(cd $(dirname $0) && pwd)
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <host>"
     exit 1
@@ -8,4 +10,4 @@ HOST=$1
 
 IMAGE=ubuntu1404.img
 
-./easy_deploy.py --use-ramdisk --no-hostname -c 4 -m 8 -i pub $HOST $IMAGE
+$TOP_DIR/easy_deploy.py --use-ramdisk --no-hostname -c 4 -m 8 -i pub $HOST $IMAGE
